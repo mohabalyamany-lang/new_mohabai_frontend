5847e807c12948f94c0d6d2751993dc0019c1d13
mohabalyamany-lang <mohabalyamany@gmail.com>
2026-04-09 20:52:27 +0200

    Create api.ts

---
+++ src/types/api.ts
@@ -0,0 +1,11 @@
+export interface ApiError {
+  detail: string;
+  status: number;
+}
+
+export interface PaginatedResponse<T> {
+  items: T[];
+  total: number;
+  page: number;
+  size: number;
+}
