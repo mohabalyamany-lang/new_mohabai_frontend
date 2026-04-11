ca9c7fc5ee097f963dca734e55c88c55195c3fd8
mohabalyamany-lang <mohabalyamany@gmail.com>
2026-04-11 16:57:53 +0200

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,9 +6,11 @@ export const tokenStorage = {
     return localStorage.getItem(ACCESS_TOKEN_KEY);
   },
   set: (token: string): void => {
+    if (typeof window === "undefined") return;
     localStorage.setItem(ACCESS_TOKEN_KEY, token);
   },
   clear: (): void => {
+    if (typeof window === "undefined") return;
     localStorage.removeItem(ACCESS_TOKEN_KEY);
   },
 };
